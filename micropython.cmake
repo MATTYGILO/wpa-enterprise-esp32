70f396c0b64539bc61e92447f0210c967f3c4715
MATTYGILO <mattygilo1@gmail.com>
2022-06-21 14:16:48 +0100

    Updated micropython make files

--- micropython.cmake
+++ micropython.cmake
@@ -1,11 +1,11 @@
-add_library(usermod_ufastecdsa INTERFACE)
+add_library(usermod_enterprise INTERFACE)
 
-target_sources(usermod_ufastecdsa INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/moducrypto.c
+target_sources(usermod_enterprise INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/enterprise.c
 )
 
-target_include_directories(usermod_ufastecdsa INTERFACE
+target_include_directories(usermod_enterprise INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-target_link_libraries(usermod INTERFACE usermod_ufastecdsa)
+target_link_libraries(usermod INTERFACE usermod_enterprise)
