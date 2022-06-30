3b6fdb5a51ae4ba1944899989c6883b120e05c18
MATTYGILO <mattygilo1@gmail.com>
2022-06-30 12:53:26 +0100

    Building without the files arround

--- enterprise.c
+++ enterprise.c
@@ -16,7 +16,7 @@
 #include "py/runtime.h"
 #include "py/objstr.h"
 
-//#include "esp_wpa2.h"
+#include "esp_wpa2.h"
 //#include "esp32/include/esp_err.h"
 //#include "micropython/ports/esp32/modnetwork.h"
 
@@ -145,23 +145,23 @@ STATIC mp_obj_t enterprise_connect(mp_obj_t username, mp_obj_t password) { // mp
     printf(EAP_IDENTITY);
     printf(EAP_PASSWORD);
 
-////    esp_exceptions();
-//    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-//
-////    esp_exceptions();
-//    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-//
-//    //esp_exceptions(esp_wifi_sta_wpa2_ent_set_new_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)));
-//
-////    esp_exceptions();
-//    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
-//
-//    // Set wpa2 wifi configuration to default settings
-//    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
-//
-//    // Set configuration settings to enable function
-////    esp_exceptions();
-//    esp_wifi_sta_wpa2_ent_enable(&config);
+//    esp_exceptions();
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+
+//    esp_exceptions();
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+
+    //esp_exceptions(esp_wifi_sta_wpa2_ent_set_new_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)));
+
+//    esp_exceptions();
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+
+    // Set wpa2 wifi configuration to default settings
+    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
+
+    // Set configuration settings to enable function
+//    esp_exceptions();
+    esp_wifi_sta_wpa2_ent_enable(&config);
 
     return mp_const_none;
 }
